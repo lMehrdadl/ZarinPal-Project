a00348ca1a03b1dca91c0d748d1e995400ca3c3d
Mehrdad Maghsoudi <Maghsoodi2002@gmail.com>
2025-10-28 19:38:02 +0330

    the dropdowns of the header have been added and there was a fixation in the actions of hamburgermenu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const crossIcon = document.querySelector('.cross-icon');
-const showMoreMenu = document.querySelector('#show-more-menu');
-const showProductMenu = document.querySelector('#show-product-menu');
+const showMoreMenu = document.querySelector('#more-menue');
+const showProductMenu = document.querySelector('#more-product-menue');
 
 hamburgerIcon.addEventListener('click', function () {
   hamburgerMenu.classList.add('show-hamburger-menu');
@@ -12,24 +12,24 @@ crossIcon.addEventListener('click', function () {
   hamburgerMenu.classList.remove('show-hamburger-menu');
 });
 
-// showMoreMenu.addEventListener('mouseenter', function (e) {
-//   const ul = e.target.childNodes[5];
-//   ul.style.animation = 'showUp 0.35s 1';
-//   ul.style.display = 'block';
-// });
+showMoreMenu.addEventListener('mouseenter', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.animation = 'showUp 0.35s 1';
+  ul.style.display = 'block';
+});
 
-// showMoreMenu.addEventListener('mouseleave', function (e) {
-//   const ul = e.target.childNodes[5];
-//   ul.style.display = 'none';
-// });
+showMoreMenu.addEventListener('mouseleave', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.display = 'none';
+});
 
-// showProductMenu.addEventListener('mouseenter', function (e) {
-//   const div = e.target.childNodes[5];
-//   div.style.animation = 'showUp 0.35s 1';
-//   div.style.display = 'flex';
-// });
+showProductMenu.addEventListener('mouseenter', function (e) {
+  const div = e.target.childNodes[5];
+  div.style.animation = 'showUp 0.35s 1';
+  div.style.display = 'flex';
+});
 
-// showProductMenu.addEventListener('mouseleave', function (e) {
-//   const div = e.target.childNodes[5];
-//   div.style.display = 'none';
-// });
+showProductMenu.addEventListener('mouseleave', function (e) {
+  const div = e.target.childNodes[5];
+  div.style.display = 'none';
+});
